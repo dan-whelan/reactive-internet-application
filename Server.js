fe248b3086b69803dd5dfe6f049f4b4316f7347d
Daniel Whelan <dpw0990@gmail.com>
2022-10-16 22:25:28 +0100

    Connected Client and Server, beginning parsing of JSON on Client side

--- Server.js
+++ Server.js
@@ -1,60 +1,182 @@
 "use strict"
 
 const express = require("express");
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
+const cors = require("cors");
 
 //set up express server
 const application = express();
 const port = 3000;
-const apiKey = "" // <- Input openweathermap.org api key here.
+const apiKey = "57ad7a485e738ee8fce6a4c886376c48" // <- Input openweathermap.org api key here.
 
 // values relating to information regarding days.
-const numEntriesInFourDays = 40
 const numEntriesPerDay = 8;
 const numDays = 5;
+const numEntriesInAllDays = numEntriesPerDay*numDays
+const numEntriesPerHalfDay = (numEntriesPerDay/2)-1
 
- // Temperature values
-const currTemp = { name: 'Temperature' };
-const minTemp = { name: 'Minimum Temperature'};
-const maxTemp = { name: 'Maximum Temperature'};
-const humidity = { name: 'Humidity' };
+const weatherMap = new Map();
 
-// Weather Conditions
-const weatherConditions = { name: 'Weather Conditions' };
-const weatherDescription = { name: 'Weather Description'};
+// Take Json from openweather and convert to Response Map
+const formatWeatherJson = async (weatherJson) => {
+    for(let i = 0; i < numEntriesInAllDays; i += numEntriesPerDay) {
+        weatherMap.set(`day_${(i/numEntriesPerDay)+1}`, getDailyInfo(i, weatherJson));
+    }
+    return weatherMap;
+}
 
-//Cloud Coverage
-const cloudCoverage = { name: 'Cloud Coverage' };
+// Get Day Info fro a given day from an openweather Json
+const getDailyInfo = (index, weatherJson) => {
+    const dayMap = new Map();
+    dayMap.set("temperature", getTemperatureInfo(index, weatherJson));
+    dayMap.set("wind", getWindInfo(index, weatherJson));
+    dayMap.set("weather", getWeatherInfo(index, weatherJson));
+    dayMap.set("cloud_coverage", getCloudInfo(index, weatherJson));
 
-//Wind Conditions
-const windSpeed = { name: 'Wind Speed'};
-const windDirection = { name: 'Wind Direction' };
+    return dayMap;
+}
 
-//Rain Conditions
-const chanceOfRain = { name: 'Chance of Rain' };
+// Get Temperature Information
+const getTemperatureInfo = (index, weatherJson) => {
+    // - Get Average Temperature for Day
+    const temperatureMap = new Map()
 
+    let averageTemp = 0;
+    for(let i = index; i < (index + numEntriesPerDay); i++) {
+        averageTemp += weatherJson.list[i].main.temp;
+        if(i === (index+numEntriesPerDay)-1)
+            averageTemp /= numEntriesPerDay;
+    }
+    temperatureMap.set("avg_temp", averageTemp);
 
-const formatWeatherJson = (weatherJson) => {
+    // - Get Max Temperature
+    let maxTemp = - Number.MAX_VALUE;
+    for(let i = index; i < (index + numEntriesPerDay); i++) {
+        if(maxTemp <= weatherJson.list[i].main.temp_max)
+            maxTemp = weatherJson.list[i].main.temp_max;
+    }
+    temperatureMap.set("max_temp", maxTemp)
+
+    // - Get Min Temperature
+    let minTemp = Number.MAX_VALUE;
+    for(let i = index; i < (index + numEntriesPerDay); i++) {
+        if(minTemp >= weatherJson.list[i].main.temp_min)
+            minTemp = weatherJson.list[i].main.temp_min;
+    }
+    temperatureMap.set("min_temp", minTemp);
+
+    // - Get average humidity
+    let humidity = 0;
+    for(let i = index; i < (index + numEntriesPerDay); i++) {
+        humidity += weatherJson.list[i].main.humidity;
+        if(i === (index + numEntriesPerDay)-1) {
+            humidity /= numEntriesPerDay;
+        }
+    }
+    temperatureMap.set("humidity", humidity);
+
+    return temperatureMap
+}
+
+// Get Wind Information
+const getWindInfo = (index, weatherJson) => {
+    // - Wind Speed
+    let windSpeed = 0;
+    for(let i = index; i < (index + numEntriesPerDay); i++) {
+        windSpeed += weatherJson.list[i].wind.speed;
+        if(i === (index+numEntriesPerDay)-1)
+            windSpeed /= numEntriesPerDay;
+    }
+    return (((windSpeed*1000)/60)/60)
+}
+
+// Get Weather Information
+const getWeatherInfo = (index, weatherJson) => {
+    // - Weather type and description
+    const dailyWeather = new Map();
+    const morningMap = new Map();
+    const afternoonMap = new Map();
+    const eveningMap = new Map();
+
+    dailyWeather.set("morning", morningMap.set(
+            "type", weatherJson.list[index].weather[0].main
+        ).set(
+            "description", weatherJson.list[index].weather[0].description
+        )
+    ).set(
+        "afternoon", afternoonMap.set(
+            "type", weatherJson.list[index+numEntriesPerHalfDay].weather[0].main
+        ).set(
+            "description", weatherJson.list[index+numEntriesPerHalfDay].weather[0].description
+        )
+    ).set(
+        "evening", eveningMap.set(
+            "type", weatherJson.list[index+(numEntriesPerDay-1)].weather[0].main
+        ).set(
+            "description", weatherJson.list[index+(numEntriesPerDay-1)].weather[0].description
+        )
+    );
+
+    return dailyWeather;
+}
+
+// Get Cloud Information
+const getCloudInfo = (index, weatherJson) => {
+    //Get Cloud Coverage
+    let cloudCoverage = 0;
+    for(let i = index; i < (index+numEntriesPerDay); i++) {
+        cloudCoverage += weatherJson.list[i].clouds.all;
+        if(i == (index+numEntriesPerDay)-1){
+            cloudCoverage /= numEntriesPerDay;
+        }
+    }
+
+    return cloudCoverage;
+}
+// Get Rainfall level average
+// const getRainfallLevel = (index, weatherJson) => {
+//     let rainfallLevel = 0;
+//     for(let i = index; i < (index+numEntriesPerDay); i++) {
+//         rainfallLevel += weatherJson.list[i].rain.3h;
+//         if(i === (index+numEntriesPerDay)-1) {
+//             rainfallLevel /= numEntriesPerDay;
+//         }
+//     }
     
-}
+//     return rainfallLevel;
+// }
 
-const getDailyInfo = () => {
+// Convert from Map to Json
+const toJson = (map = new Map) =>
+    Object.fromEntries
+        ( Array.from
+            ( map.entries()
+            , ([ k, v ]) =>
+                v instanceof Map
+                ? [ k, toJson(v) ]
+                : [ k, v ]
+            )
+        );
 
-}
-
-application.get("/weather/:cityName", async (req, res) => {
+// Server Endpoint for Weather    
+application.get("/weather/:cityName", cors(), async (req, res) => {
     try {
+        console.log("request received");
         const location = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${req.params.cityName}&limit=1&appId=${apiKey}`);
         const locationJson = await location.json();
+        console.log("location parsed");
         const weather = 
             await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${locationJson[0].lat}&lon=${locationJson[0].lon}&appId=${apiKey}&units=metric`);
         const weatherJson = await weather.json();
-        const formattedWeatherJson = formatWeatherJson(weatherJson);
-        res.status(200).json(formattedWeatherJson);
-    } catch {
+        console.log("weather parsed");
+        const formattedWeatherMap = await formatWeatherJson(weatherJson);
+        console.log("weather formatted");
+        res.status(200).json(toJson(formattedWeatherMap));
+        console.log("Response Sent");
+    } catch (err) {
         console.log(err);
-        res.status(500).json({msg: `Internal Server Error.`})    
+        res.status(500).json({msg: `Internal Server Error.`});    
     }
-})
+});
 
 application.listen(port, () => console.log(`The app is listening on port: ${port}`));
